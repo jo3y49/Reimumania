c9cd8657f0af71a196a81eebe9135a6a88de2b17
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-06-01 13:47:41 -0700

    added adjustable camera, mostly for debugging

---
+++ Touhou_Game/Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minOrtho = 1f;
+    public float midOrtho = 10f;
+    public float maxOrtho = 20f;
+    public KeyCode cameraToggle = KeyCode.Tab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(cameraToggle))
+        {
+            float orthoSize = Camera.main.orthographicSize;
+
+            if (Camera.main.orthographicSize == minOrtho)
+            {
+                orthoSize = midOrtho;
+            } else if (Camera.main.orthographicSize == midOrtho)
+            {
+                orthoSize = maxOrtho;
+            } else 
+            {
+                orthoSize = minOrtho;
+            }
+
+            Camera.main.orthographicSize = Mathf.Clamp(orthoSize, minOrtho, maxOrtho);
+        }
+    }
+}
