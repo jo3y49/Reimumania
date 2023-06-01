3a9a037c1164d2109296471eb0c388ef431a8e11
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-06-01 13:53:05 -0700

    added simple circle movement to enemy

---
+++ Touhou_Game/Assets/Scripts/Enemies/Movement Patterns/CirclePatrol.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CirclePatrol : MonoBehaviour {
+    public float patrolRadius = 1f; // The radius of the circular path
+    public float patrolSpeed = 1f; // How fast the enemy moves along the path
+
+    private float patrolAngle = 0f; // The current angle along the circular path
+
+    private void Update() {
+        // Move along the circular patrol path
+        patrolAngle += patrolSpeed * Time.deltaTime;
+        Vector3 patrolOffset = new Vector3(Mathf.Cos(patrolAngle), Mathf.Sin(patrolAngle), 0) * patrolRadius;
+        transform.position += patrolOffset / 500f;
+    }
+
+}
