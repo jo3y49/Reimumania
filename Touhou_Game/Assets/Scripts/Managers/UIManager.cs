4bfb9f500c717f6d66aec27d42186e257fbf1314
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-06-04 12:57:22 -0700

    fixed webgl quirks

--- Touhou_Game/Assets/Scripts/Managers/UIManager.cs
+++ Touhou_Game/Assets/Scripts/Managers/UIManager.cs
@@ -19,7 +19,7 @@ public class UIManager : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.P)) {
             if (isPaused) {
                 Resume();
             } else {
